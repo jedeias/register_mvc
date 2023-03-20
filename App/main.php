3e11abb68dd9ceb27e67d25f5cf0e13733c682b6
jedeias_Luiz <jedeiasluiz3@gmail.com>
2023-03-19 23:51:58 -0300

    changed index html to index php created a class person

--- App/main.php
+++ App/main.php
@@ -10,5 +10,9 @@ $controller->show_test = new View_test();
 
 $controller->show_test->test();
 
+$obj = new Person("tom", "jobin","M","1994-04-05","01234567890","teste@teste.com", "password");
+
+echo"<pre>";
+print_r ($obj);
 
 ?>
